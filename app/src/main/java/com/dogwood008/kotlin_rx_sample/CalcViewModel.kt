feaa201f014fd47ad427567f2854fe43ded8d6f2
dogwood008 <dogwood008+github@gmail.com>
2018-09-15 16:58:10 +0900

    initial commit

---
+++ app/src/main/java/com/dogwood008/kotlin_rx_sample/CalcViewModel.kt
@@ -0,0 +1,7 @@
+package com.dogwood008.kotlin_rx_sample
+
+import android.arch.lifecycle.ViewModel
+
+class CalcViewModel : ViewModel() {
+    var display = 0
+}
---
+++ app/src/main/java/com/dogwood008/kotlin_rx_sample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.dogwood008.kotlin_rx_sample
+
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: CalcViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this!!).get(CalcViewModel::class.java)
+        displayTextView.text = viewModel.display.toString()
+    }
+}
