13380eda6b88edb5bc3e6a5658bd749aa12f8987
dogwood008 <dogwood008+github@gmail.com>
2018-09-19 11:50:25 +0900

    :+1: add events

--- app/src/main/java/com/dogwood008/kotlinrxsample/MainActivity.kt
+++ app/src/main/java/com/dogwood008/kotlinrxsample/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.dogwood008.kotlinrxsample
 
 import android.arch.lifecycle.ViewModelProviders
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.jakewharton.rxbinding2.view.RxView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
     private lateinit var viewModel: CalcViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +19,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProviders.of(this).get(CalcViewModel::class.java)
         displayTextView.text = viewModel.display.toString()
+        setEvents()
+    }
+
+    private fun setEvents() {
+        val clickTenKey0Observable = RxView.clicks(tenKey0Button)
+        clickTenKey0Observable
+                .subscribe{ showToast(0) }
+    }
+
+    private fun showToast(text: Any) {
+        Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        Log.d(TAG, text.toString())
     }
 }
