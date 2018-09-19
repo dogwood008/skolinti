c13a642b4940d0ac597b095bcc8c9474479a4574
dogwood008 <dogwood008+github@gmail.com>
2018-09-19 11:26:05 +0900

    :+1: add button

--- app/src/main/java/com/dogwood008/kotlinrxsample/MainActivity.kt
+++ app/src/main/java/com/dogwood008/kotlinrxsample/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this!!).get(CalcViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(CalcViewModel::class.java)
         displayTextView.text = viewModel.display.toString()
     }
 }
