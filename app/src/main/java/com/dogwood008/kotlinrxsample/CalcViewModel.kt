9edda797eb7b8b2b9f163ec58d73db78e32337ad
dogwood008 <dogwood008+github@gmail.com>
2018-09-26 00:23:24 +0900

    :+1: use rx to update display & event

--- app/src/main/java/com/dogwood008/kotlinrxsample/CalcViewModel.kt
+++ app/src/main/java/com/dogwood008/kotlinrxsample/CalcViewModel.kt
@@ -1,7 +1,28 @@
 package com.dogwood008.kotlinrxsample
 
-import android.arch.lifecycle.ViewModel
+import android.databinding.BaseObservable
+import android.databinding.Bindable
+import android.databinding.ObservableInt
+import android.util.Log
+import rx.subjects.PublishSubject
 
-class CalcViewModel : ViewModel() {
-    var display = 0
+class CalcViewModel : BaseObservable() {
+    companion object {
+        private val TAG = CalcViewModel::class.java.simpleName
+    }
+
+    private val tenKeySubject = PublishSubject.create<Int>()
+    val tenKeyObservable = tenKeySubject.asObservable()!!
+
+    @get:Bindable
+    var display = ObservableInt(0)
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.display)
+        }
+
+    fun onClickTenKey(num: Int) {
+        Log.d(TAG, num.toString())
+        tenKeySubject.onNext(num)
+    }
 }
--- app/src/main/java/com/dogwood008/kotlinrxsample/MainActivity.kt
+++ app/src/main/java/com/dogwood008/kotlinrxsample/MainActivity.kt
@@ -1,46 +1,33 @@
 package com.dogwood008.kotlinrxsample
 
-import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
-import com.jakewharton.rxbinding2.view.RxView
-import com.jakewharton.rxbinding2.widget.RxTextView
-import io.reactivex.Observable
-import kotlinx.android.synthetic.main.activity_main.*
+import com.dogwood008.kotlinrxsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private val TAG = MainActivity::class.java.simpleName
     }
 
-    private lateinit var viewModel: CalcViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this).get(CalcViewModel::class.java)
-        displayTextView.text = viewModel.display.toString()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = CalcViewModel()
         setEvents()
     }
 
     private fun setEvents() {
-        val keys = mutableMapOf(
-                tenKey0Button to 0, tenKey1Button to 1, tenKey2Button to 2,
-                tenKey3Button to 3, tenKey4Button to 4, tenKey5Button to 5,
-                tenKey6Button to 6, tenKey7Button to 7, tenKey8Button to 8,
-                tenKey9Button to 9)
-        for (kv in keys) {
-            RxView.clicks(kv.key)
-                    .flatMap { Observable.fromCallable { kv.value } }
-                    .subscribe { appendNum(it) }
-        }
-    }
-
-    private fun appendNum(num: Int) {
-        viewModel.display += num
-        RxTextView.text(displayTextView).accept(viewModel.display.toString())
+        binding.viewModel!!.tenKeyObservable
+                .doOnNext {
+                    val prevValue = binding.viewModel!!.display.get()
+                    binding.viewModel!!.display.set(it + prevValue)
+                }
+                .subscribe()
     }
 
     private fun showToast(text: Any) {
